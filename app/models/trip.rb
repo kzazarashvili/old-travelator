febae0863cd086e6010117c62cada70ab8e289a0
Patryk <patryk.mrozinski.pm@gmail.com>
2019-04-03 17:19:52 +0100

    before create callback in Trip model for set up duration of trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,14 @@
 class Trip < ApplicationRecord
   belongs_to :user
   validates :started_at, presence: true
+  before_create :number_of_days
+
+
+  def number_of_days
+    if ended_at.present?
+      self.duration = (ended_at - started_at).to_i
+    else
+      self.duration = 0
+    end
+  end
 end
