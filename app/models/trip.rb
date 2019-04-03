5a5f58f990a8714cd4a5cff908d21c714f6a19b9
Patryk <patryk.mrozinski.pm@gmail.com>
2019-04-03 19:02:58 +0100

    update for Trip model with before save callback

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,20 @@ class Trip < ApplicationRecord
   belongs_to :user
 
   validates :started_at, presence: true
+
+  before_save :calculate_and_set_duration, if: :ending_date_known?
+
+  private
+
+  def calculate_and_set_duration
+    self.duration = calculate_duration
+  end
+
+  def ending_date_known?
+    ended_at.present?
+  end
+
+  def calculate_duration
+    (ended_at - started_at).to_i
+  end
 end
