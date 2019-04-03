dfb66b5498ca492a8a08a8029850c56610b9051d
Patryk <patryk.mrozinski.pm@gmail.com>
2019-04-03 17:46:16 +0100

    Create Trip model and association with User

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_195159) do
+ActiveRecord::Schema.define(version: 2019_04_03_163131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.date "started_at", null: false
+    t.date "ended_at"
+    t.integer "duration", default: 0
+    t.bigint "user_id"
+    t.boolean "past", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_03_24_195159) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "trips", "users"
 end
