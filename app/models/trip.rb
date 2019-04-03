93f4c0ec3a149817b27ca1d4d99108a8de58edf9
Patryk <patryk.mrozinski.pm@gmail.com>
2019-04-03 18:03:31 +0100

    Add before create rollback with method seting up duration

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,8 @@
 class Trip < ApplicationRecord
   belongs_to :user
+  before_create :number_of_days
+
+  def number_of_days
+    self.duration = (ended_at - started_at).to_i if ended_at.present?
+  end
 end
