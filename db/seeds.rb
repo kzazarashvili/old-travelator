cecb717022fd82a5bc50a2c3b0f75667a0cdffd9
Patryk <patryk.mrozinski.pm@gmail.com>
2019-04-19 20:31:14 +0100

    create seeds for countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+country = Country.create([{ name: 'Poland' }])
+country1 = Country.create([{ name: 'Germany' }])
+country2 = Country.create([{ name: 'Italy' }])
+country3 = Country.create([{ name: 'Netherlands' }])
+country4 = Country.create([{ name: 'Spain' }])
+country5 = Country.create([{ name: 'Potugal' }])
+country6 = Country.create([{ name: 'France' }])
