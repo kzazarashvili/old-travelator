9625b99bca53e4231cebd9ee71fbba4cadfef7a1
Patryk <patryk.mrozinski.pm@gmail.com>
2019-05-24 00:41:36 +0100

    Create admin panel with possibility create, edit, delete countries

---
+++ app/controllers/admin/countries_controller.rb
@@ -0,0 +1,52 @@
+module Admin
+  class CountriesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_country, only: %i[show edit update destroy]
+
+    def index
+      @countries = Country.all
+      @country = Country.new
+    end
+
+    def new
+      @country = Country.new
+    end
+
+    def create
+      @country = Country.new(country_params)
+
+      if @country.save
+        redirect_to [:admin, @country]
+      else
+        render :new, alert: 'Country could not be saved'
+      end
+    end
+
+    def show; end
+
+    def edit; end
+
+    def update
+      if @country.update(country_params)
+        redirect_to [:admin, @country]
+      else
+        render :edit, alert: 'Country could not be updated'
+      end
+    end
+
+    def destroy
+      @country.destroy
+      redirect_to admin_countries_path
+    end
+
+    private
+
+    def set_country
+      @country = Country.find(params[:id])
+    end
+
+    def country_params
+      params.require(:country).permit(:name)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :trips
+
+  authenticate :user, lambda { |u| u.admin? } do
+
+    get '/admin', to: 'admin/countries#index'
+
+    namespace :admin do
+      resources :countries
+    end
+  end
 end
