72a5a33c7151f7a518ebf8a9e1dd67772e20c670
Ketevan Zazarashvili <ketevanzazarashvili.kz@gmail.com>
2019-05-28 22:48:36 +0100

    Admin::BaseHelper issue fixes

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -2,14 +2,16 @@ module Admin
   module BaseHelper
     def order_by_link(name, attribute = nil)
       attribute ||= name.parameterize(separator: '_')
+
+      class_name = "-#{order_by_direction(attribute)}" if order_by_direction(attribute).present?
+
       options = {
         order: attribute,
         direction: order_by_direction(attribute) == 'asc' ? 'desc' : 'asc'
       }
-      class_name = "-#{order_by_direction(attribute)}" unless order_by_direction(attribute).blank?
 
       link_to params.to_unsafe_h.merge(options) do
-        raw name + content_tag(:i, nil, class: "fa fa-sort#{class_name}")
+        safe_join([name, content_tag(:i, nil, class: "fa fa-sort#{class_name}")])
       end
     end
 
